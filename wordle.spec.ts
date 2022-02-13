518f1a1dfc48978c8a28958b3b66067240919bc2
Sammy Hass <sammyhass01@gmail.com>
2022-02-13 13:18:42 +0000

    no real need to accept cookies

--- wordle.spec.ts
+++ wordle.spec.ts
@@ -5,11 +5,7 @@ import { uploadResult } from './lib/uploadResult';
 test('get wordle result', async ({ page }) => {
   await page.goto(WORDLE_URL);
 
-  // Accept Cookies
-  await page.waitForSelector('#pz-gdpr-btn-accept');
-  await page.click('#pz-gdpr-btn-accept');
-
-  let sol;
+  await page.waitForLoadState('networkidle');
 
   const result = await page.evaluate(() => {
     // @ts-ignore
@@ -19,8 +15,6 @@ test('get wordle result', async ({ page }) => {
     const solution = game.solution;
     return solution as string;
   });
-
-  await page.waitForTimeout(4000);
 
   // Create a new gh issue with the solution
   await uploadResult(result);
