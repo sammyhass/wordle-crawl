65c50ad6d611ba82c58dfc803a12b60912ff29f6
Sammy Hass <sammyhass01@gmail.com>
2022-02-13 14:04:54 +0000

    better formatting of issues

--- constants.ts
+++ constants.ts
@@ -3,3 +3,7 @@ export const WORDLE_URL = 'https://www.nytimes.com/games/wordle/index.html';
 export const REPO_NAME = process.env.REPO_NAME;
 export const REPO_OWNER = process.env.REPO_OWNER;
 export const GITHUB_TOKEN = process.env.GH_TOKEN;
+
+// WORDLE DAY
+// On Feb 13 2022 it was wordle day 239, so we'll count from there
+export const WORDLE_DAY = new Date(2022, 2, 13).getDay() + 239;
--- lib/uploadResult.ts
+++ lib/uploadResult.ts
@@ -1,18 +1,18 @@
 import { Octokit } from '@octokit/rest';
-import { GITHUB_TOKEN, REPO_NAME, REPO_OWNER } from '../constants';
+import { GITHUB_TOKEN, REPO_NAME, REPO_OWNER, WORDLE_DAY } from '../constants';
 
 const octo = new Octokit({ auth: GITHUB_TOKEN });
 
 export const uploadResult = async (solution: string) => {
   const date = new Date();
 
-  const wordleString = `🟩🟩🟩🟩🟩`;
+  const wordleString = `1/6 🟩 🟩 🟩 🟩 🟩`;
 
   const { data, status } = await octo.issues.create({
     owner: REPO_OWNER || '',
     repo: REPO_NAME || '',
-    title: `${date.toDateString()}`,
-    body: `## Worldle Solution for ${date.toDateString()}\n${wordleString}\n## Solution: **${solution}**`,
+    title: `Wordle Solution ${WORDLE_DAY} - ${date.toDateString()}`,
+    body: `## Wordle ${WORDLE_DAY}\n${wordleString}\n### \`solution: ${solution}\``,
     labels: ['solution', 'wordle', date.toDateString()],
   });
 
