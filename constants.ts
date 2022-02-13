6ce04245fad5d7068cd4c339deff7bf0a0c3657e
Sammy Hass <sammyhass01@gmail.com>
2022-02-13 12:28:19 +0000

    fix env vars

--- constants.ts
+++ constants.ts
@@ -2,4 +2,4 @@ export const WORDLE_URL = 'https://www.nytimes.com/games/wordle/index.html';
 
 export const REPO_NAME = process.env.REPO_NAME;
 export const REPO_OWNER = process.env.REPO_OWNER;
-export const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+export const GITHUB_TOKEN = process.env.GH_TOKEN;
